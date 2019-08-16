7aced2491ccb3db90c17ad44f9874916f089085d
MachinisteWeb <bruno.lesieur@gmail.com>
2019-08-16 10:05:46 +0200

    Change the Unit test to pass it successfully
    Signed-off-by: MachinisteWeb <bruno.lesieur@gmail.com>

--- regular-expression-url/controllers/members.js
+++ regular-expression-url/controllers/members.js
@@ -1,26 +1,24 @@
-exports.changeVariations = function (params, next) {
-    var variations = params.variations,
-    	request = params.request;
+exports.changeVariations = function (next, locals, request, response) {
 
     console.log("param request:", request.params.member);
     // \> undefined, 'toto', 'bob-eponge99', 'node-atlas' or 'etc'.
-    console.log("param variation:", variations.params.member);
+    console.log("param variation:", locals.params.member);
     // \> undefined, 'toto', 'bob-eponge99', 'node-atlas' or 'etc'.
 
     console.log("param request", request.params.action);
     // \> undefined, 'show' or 'lolol'.
-    console.log("param variation", variations.params.action);
+    console.log("param variation", locals.params.action);
     // \> undefined, 'show' or 'lolol'.
 
     console.log("query request", request.query.example);
     // \> undefined or 'test'
-    console.log("query variation", variations.query.example);
+    console.log("query variation", locals.query.example);
     // \> undefined or 'test'
 
     console.log("body request", request.body.test);
     // \> undefined or 'This is a test'.
-    console.log("body variation", variations.body.test);
+    console.log("body variation", locals.body.test);
     // \> undefined or 'This is a test'.
 
-    next(variations);
+    next();
 };
