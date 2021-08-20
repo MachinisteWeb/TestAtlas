234b67f6071b73e6c14b6635cf78ceb1a142cf36
MachinisteWeb <bruno.lesieur@gmail.com>
2021-08-20 15:26:09 +0200

    Fix security issue with express-jwt
    Signed-off-by: MachinisteWeb <bruno.lesieur@gmail.com>

--- vs-sails/nodeatlas/api/middlewares/is-authenticated.js
+++ vs-sails/nodeatlas/api/middlewares/is-authenticated.js
@@ -3,7 +3,8 @@ var jwt = require("express-jwt");
 module.exports = function () {
 	return [jwt({
   		secret: "7CyDzecU_dwsNuKeNb1K7d2Ktf4RYWFqqXoeV61Cb_qfzaEU_HHY5vETMSH3nx85",
-		audience: "NQ8SuASOanVk8LdLru3Syg3uTWUbBWvB"
+		audience: "NQ8SuASOanVk8LdLru3Syg3uTWUbBWvB",
+		algorithms: ['HS256']
 	}), function (error, request, response, next) {
 		if (error.name === "UnauthorizedError") {
     		return response.status(401).send("Invalid Token...");
--- vs-sails/sails/api/api/policies/isAuthenticated.js
+++ vs-sails/sails/api/api/policies/isAuthenticated.js
@@ -1,7 +1,8 @@
 var jwt = require("express-jwt"),
 	authCheck = jwt({
   		secret: "7CyDzecU_dwsNuKeNb1K7d2Ktf4RYWFqqXoeV61Cb_qfzaEU_HHY5vETMSH3nx85",
-		audience: "NQ8SuASOanVk8LdLru3Syg3uTWUbBWvB"
+		audience: "NQ8SuASOanVk8LdLru3Syg3uTWUbBWvB",
+		algorithms: ['HS256']
 	});
 
 module.exports = authCheck;
